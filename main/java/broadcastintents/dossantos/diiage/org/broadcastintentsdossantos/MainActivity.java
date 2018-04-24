0889ddd3b0e021df822b2ba2036881d62ebbd86b
QuentinDOSSANTOS <quentin.dossantos@yandex.com>
2018-04-24 20:15:47 +0200

    Initial commit

---
+++ main/java/broadcastintents/dossantos/diiage/org/broadcastintentsdossantos/MainActivity.java
@@ -0,0 +1,18 @@
+package broadcastintents.dossantos.diiage.org.broadcastintentsdossantos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ main/java/broadcastintents/dossantos/diiage/org/broadcastintentsdossantos/SmsReceiver.java
@@ -0,0 +1,43 @@
+package broadcastintents.dossantos.diiage.org.broadcastintentsdossantos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+/**
+ * Created by Quentin on 24/04/2018.
+ */
+
+public class SmsReceiver extends BroadcastReceiver {
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(SMS_RECEIVED)) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                // get sms objects
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                if (pdus.length == 0) {
+                    return;
+                }
+                // large message might be broken into many
+                SmsMessage[] messages = new SmsMessage[pdus.length];
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    sb.append(messages[i].getMessageBody());
+                }
+                String message = sb.toString();
+
+                //Affiche le sms dans un toast
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.show();
+            }
+        }
+    }
+}
